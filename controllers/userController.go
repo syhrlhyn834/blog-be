ae01d12152efb614830fc67d6f22854bd4c5b501
Syahril Haryono <syhrlhyn@gmail.com>
2024-11-29 20:08:16 +0700

    Update Minimum Password

--- controllers/userController.go
+++ controllers/userController.go
@@ -12,7 +12,7 @@ import (
 type ValidateUserInput struct {
 	Name     string `josn:"name" binding:"required"`
 	Email    string `json:"email" binding:"required,email"`
-	Password string `json:"password" binding:"required"`
+	Password string `json:"password" binding:"required,min=6"`
 }
 
 func Findusers(c *gin.Context) {
