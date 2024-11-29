3d1018e0e22955f165df48c3a0bbc2815993690f
Syahril Haryono <syhrlhyn@gmail.com>
2024-11-29 15:54:54 +0700

    Update User Models: hidden password

--- models/user.go
+++ models/user.go
@@ -6,7 +6,7 @@ type User struct {
 	Id        int        `json:"id" gorm:"primary_key"`
 	Name      string     `json:"name"`
 	Email     string     `json:"email" gorm:"unique"`
-	Password  string     `json:"password"`
+	Password  string     `json:"-"`
 	CreatedAt *time.Time `json:"created_at"`
 	UpdatedAt *time.Time `json:"updated_at"`
 }
