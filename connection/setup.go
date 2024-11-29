b9c5d333b23f9955eb9771d4c15b0c948bbe6fb9
Syahril Haryono <syhrlhyn@gmail.com>
2024-11-29 15:28:24 +0700

    Add Connection and Migration

---
+++ connection/setup.go
@@ -0,0 +1,32 @@
+package connection
+
+import (
+	"be-golang/models"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDatabase() {
+
+	// Koneksi ke database MySQL
+	database, err := gorm.Open(mysql.Open("root:@tcp(127.0.0.1:3306)/begolang?charset=utf8&parseTime=True&loc=Local"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	// Migrasi menggunakan model dari package models
+	database.AutoMigrate(
+		&models.User{},
+		&models.Category{},
+		&models.Post{},
+		&models.Menu{},
+		&models.Sosmed{},
+		&models.Dataweb{},
+	)
+
+	// Menyimpan instance koneksi database ke variabel global
+	DB = database
+}
